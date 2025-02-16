fe4b6b54173349646039deb3f35f30394efae8dc
Octaviofg <48572130+OctavioFloG@users.noreply.github.com>
2025-02-16 01:09:08 -0600

    Proyecto fany
    -No más commit, el proyecto está terminado

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// Función para mostrar el pop-up de la sorpresa
+function mostrarPopup() {
+    document.getElementById('popup').style.display = 'flex';
+  }
+  
+  // Función para cerrar el pop-up de la sorpresa
+  function cerrarPopup() {
+    document.getElementById('popup').style.display = 'none';
+  }
+  
+  // Función para mostrar el pop-up final
+  function mostrarMensajeFinal() {
+    document.getElementById('popup-final').style.display = 'flex';
+  }
+  
+  // Función para cerrar el pop-up final
+  function cerrarPopupFinal() {
+    document.getElementById('popup-final').style.display = 'none';
+  }
